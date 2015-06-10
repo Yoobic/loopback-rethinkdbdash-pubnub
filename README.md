c21b49995f2f88e5d02d2a0e917d535032a3a2d6
Avi Haiat <thaiat@ipelia.fr>
2015-06-10 19:04:31 +0300

    feat(app): Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Loopback connector for RethinkDB.
+
+[![NPM version](https://badge.fury.io/js/loopback-rethinkdbdash-pubnub.svg)](http://badge.fury.io/js/loopback-rethinkdbdash-pubnub) [![Downloads](http://img.shields.io/npm/dm/loopback-rethinkdbdash-pubnub.svg)](http://badge.fury.io/js/loopback-rethinkdbdash-pubnub)   
+[![Build Status](https://travis-ci.org/Yoobic/loopback-rethinkdbdash-pubnub.svg?branch=master)](https://travis-ci.org/Yoobic/loopback-rethinkdbdash-pubnub) [![Test Coverage](https://codeclimate.com/github/Yoobic/loopback-rethinkdbdash-pubnub/badges/coverage.svg)](https://codeclimate.com/github/Yoobic/loopback-rethinkdbdash-pubnub) [![Code Climate](https://codeclimate.com/github/Yoobic/loopback-rethinkdbdash-pubnub/badges/gpa.svg)](https://codeclimate.com/github/Yoobic/loopback-rethinkdbdash-pubnub)   
+[![Dependency Status](https://david-dm.org/Yoobic/loopback-rethinkdbdash-pubnub.svg)](https://david-dm.org/Yoobic/loopback-rethinkdbdash-pubnub) [![devDependency Status](https://david-dm.org/Yoobic/loopback-rethinkdbdash-pubnub/dev-status.svg)](https://david-dm.org/Yoobic/loopback-rethinkdbdash-pubnub#info=devDependencies) [![peerDependency Status](https://david-dm.org/Yoobic/loopback-rethinkdbdash-pubnub/peer-status.svg)](https://david-dm.org/Yoobic/loopback-rethinkdbdash-pubnub#info=peerDependencies)    
+
+A loopback connector for rethinkdb using rethinkdbdash
+
+[![NPM](https://nodei.co/npm/loopback-rethinkdbdash-pubnub.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/loopback-rethinkdbdash-pubnub)
+
+## Description
+This connector is based on rethinkdbdash driver.
+Please, report all issues, this will help to publish stable version on NPM as soon as possible.
+
+Please, note that many tests from Loopback Juggler suite are made with MongoDB in mind and RethinkDB shows different behavior in some cases.
+
+## Changelog
+
+Recent changes can be viewed on Github on the [Releases Page](https://github.com/Yoobic/loopback-rethinkdbdash-pubnub/releases)
+
+## License
+
+MIT
---
+++ gulp_tasks/common/constants.js
@@ -0,0 +1,65 @@
+/*eslint no-empty:0*/
+'use strict';
+
+var path = require('path');
+
+var getRepository = function() {
+    var repository = 'https://github.com/user/repo';
+    try {
+        var helper = require('./helper');
+        var packageJson = helper.readJsonFile('./package.json');
+        var _ = require('lodash');
+        if(_.isString(packageJson.repository)) {
+            repository = packageJson.repository.replace('.git', '');
+        } else {
+            repository = packageJson.repository.url.replace('.git', '');
+        }
+    } catch(err) {}
+    return repository;
+};
+
+var getAppname = function() {
+    var appname;
+    try {
+        var helper = require('./helper');
+        var packageJson = helper.readJsonFile('./package.json');
+        appname = packageJson.name;
+    } catch(err) {}
+    return appname;
+};
+
+module.exports = function() {
+    var cwd = process.env.INIT_CWD || '';
+    var clientFolder = 'client'; // the source file folder
+    var defaultTarget = 'app'; // the name of the app that corresponds to index.html
+    var constants = {
+        cwd: cwd,
+        appname: getAppname(),
+        defaultTarget: defaultTarget,
+        targetName: '{{targetName}}',
+        targetSuffix: '{{targetSuffix}}',
+        mode: '{{mode}}',
+        clientFolder: clientFolder,
+        repository: getRepository(),
+        versionFiles: ['./package.json', './bower.json', './' + clientFolder + '/config*.xml'],
+        growly: {
+            notify: false,
+            successIcon: path.join(cwd, 'node_modules/karma-growl-reporter/images/success.png'),
+            failedIcon: path.join(cwd, 'node_modules/karma-growl-reporter/images/failed.png')
+        },
+        lint: [
+            './*.js', 'gulpfile.js', './gulp_tasks/**/*.js', './test/**/*.js', './lib/**/*.js'
+        ],
+        mocha: {
+            libs: ['./*.js', '!gulpfile.js'],
+            tests: ['./test/**/*.js'],
+            globals: 'test/helpers/globals.js',
+            timeout: 5000
+        },
+        dist: {
+            distFolder: './dist/{{targetName}}/{{mode}}'
+        }
+    };
+
+    return constants;
+};
